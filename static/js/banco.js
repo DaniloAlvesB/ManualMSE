3a264bca2ba638bdf8e599be42e17b3e24a5f608
Danilo <danilobernardino786@gmail.com>
2023-02-01 19:30:13 -0300

    other

--- static/js/banco.js
+++ static/js/banco.js
@@ -103,4 +103,121 @@ var textos = [
     `,
     "http://www.videostutoriais.grupooportunidade.com.br/suporte/videos/sistema/4.html"
     ],
+
+
+    [`Sobre a matrícula`, `
+        <p class="mx-2">
+            Após realizar o cadastro do aluno, o próximo passo é matricular o aluno no sistema. Por
+            meio do número de sua matrícula é que o aluno conseguirá acessar o portal do aluno,
+            caso contrário ele não poderá assistir as suas aulas. É na matrícula também que são
+            inseridas informações como:
+            <ul class="mx-4">
+                <li>Módulo que o aluno cursará (módulo é o conjunto de cursos)</li>
+                <li>Início e término de contrato</li>
+                <li>Nº de horas semanais que o aluno cursará</li>
+                <li>Valor do pacote</li>
+                <li>Quantidade de parcelas</li>
+                <li>Tipo de marketing usado para captação do aluno. Entre outros</li>
+            </ul>
+        </p>
+    `, `
+        Nova matrícula, matricula, novo, Aluno, Cadastro, basico, básico
+    `,
+    "http://www.videostutoriais.grupooportunidade.com.br/suporte/videos/sistema/8.html"
+    ],
+
+
+    [`Criando matrícula do aluno`, `
+        <p class="mx-2">
+            Dentro do sistema escola <br>
+            <b class="m-2">A.</b>Clique sobre a aba MOVIMENTAÇÃO <br>
+            <b class="m-2">B.</b>Matrículas <br>
+
+            <br>
+            <img class="img-fluid my-2 img-exp" src="static/images/movimentacao.jpg" alt="Campo nome do aluno">
+            <br> <br>
+
+            <b class="m-2">C.</b>Clique sobre o botão <button class="btn btn-success text-light"><b>+</b> NOVA MATRÍCULA</button><br>
+
+            <br>
+            <img class="img-fluid my-2 img-exp" src="static/images/nova_matricula.jpg" alt="Campo nome do aluno">
+            <br> <br>
+
+            <b class="m-2">D.</b>Após clicar sobre este botão, uma nova tela irá abrir, nessa tela você deverá
+            preencher todos os campos que estiverem com o *. <br>
+
+            <br>
+            <img class="img-fluid my-2 img-exp" src="static/images/cabecalho_matricula.jpg" alt="Campo nome do aluno">
+            <br> <br>
+
+            <b class="m-2">E.</b>Ao clicar sobre a lupa na área do ALUNO, aparecerá uma lista com todos os
+            alunos cadastrados no sistema. Selecione o aluno de quem deseja fazer a
+            matrícula e em seguida selecione o módulo. <br> <br>
+            <b class="m-2">F.</b>As horas semanais significam quantas horas na semana o seu aluno terá para
+            fazer as suas aulas. Por exemplo, se o seu aluno fizer 1 hora de aula, mas 2 dias
+            na semana, então deverá ser colocado 2 horas no campo H/SEMANA. <br> <br>
+            <b class="m-2">G.</b>Consultor é a pessoa responsável pela matricula do aluno. O sistema possibilita a
+            criação de usuários para os demais funcionários, além do usuário administrativo,
+            permitindo colocar exatamente o funcionário que está fazendo a matricula do
+            aluno. <br> <br>
+            <b class="m-2">H.</b>Há duas opções de escolha para o pagamento do cliente no sistema escola.
+            Boleto normal e o boleto pagseguro. O sistema escola online possibilita a
+            geração de boletos, pagamento por cartão de crédito e débito, além da opção
+            recorrência. Para uso da opção da pagseguro é necessário a criação de uma conta
+            na pagseguro, bem como configurar o sistema com as informações necessárias. <br> <br>
+
+            O Grupo Oportunidade firmou uma parceria com a pagseguro, diminuindo o valor das
+            taxas cobradas pela empresa em caso de uso de pagamento online. Para ser inserido
+            nessas condições é necessário enviar um e-mail solicitando à pagseguro a inserção da
+            sua escola no plano. (Vide tutorial pagseguro) <br> <br>
+
+            <b class="m-2">I.</b>Existe um campo na matrícula do aluno onde é possível selecionar a opção
+            FLEX. Essa opção deve ser selecionada quando seu aluno não possuir um
+            horário fixo, ou seja, ele poderá ter horários em dias diferentes na semana. Nessa
+            opção o aluno não terá um agendamento fixo, e sim flexível. Onde será
+            necessário inserir o horário para ele de acordo com a necessidade.
+            Quando o aluno não é flex, o sistema deixa o agendamento salvo, ou seja, toda semana
+            naquele dia e horário o aluno terá o sistema aberto para que ele possa fazer aula. No
+            modelo flex, a aula precisará ser agendada semanalmente. <br> <br>
+            <b class="m-2">J.</b>Depois de inserir todos os dados, é só clicar em SALVAR NOVA
+            MATRÍCULA. Dessa forma seu aluno terá a matrícula efetuada com sucesso.
+        </p>
+    `, `
+        Nova matrícula, matricula, novo, Aluno, Cadastro
+    `,
+    "http://www.videostutoriais.grupooportunidade.com.br/suporte/videos/sistema/8.html"
+    ],
+
+
+    [`Inserindo horário na matrícula`, `
+        <p class="mx-2">
+            Após criar a matrícula o próximo passo é agendar um horário para o seu aluno. <br>
+            <b class="m-2">A.</b>Abra a matrícula do aluno clicando na lupa de ação <br> <br>
+            <b class="m-2">B.</b>Após abrir a matrícula, siga até o final da página, onde encontrará algumas
+            guias. Dentre elas estão: CURSOS, PARCELAS, HORÁRIOS, REPOSIÇÕES,
+            FREQUÊNCIAS, ALTERAÇÕES, CONTATOS ALUNO, DESCONTO. <br> <br>
+            <b class="m-2">C.</b>Para inserir o horário, clique na aba HORÁRIOS, escolha o dia em que o aluno
+            fará a aula e em seguida clique no botão + em frente ao horário para que ele seja
+            agendado para essa matrícula. <br>
+
+            <br>
+            <img class="img-fluid my-2 img-exp" src="static/images/matricula_horario.jpg" alt="Campo nome do aluno">
+            <br> <br>
+
+            <b class="m-2">D.</b>Após inserir o horário, ele irá constar na área de HORÁRIOS AGENDADOS. <br>
+            
+            <br>
+            <img class="img-fluid my-2 img-exp" src="static/images/adc_horario_matricula.jpg" alt="Campo nome do aluno">
+            <br> <br>
+
+            <b class="m-2">E.</b>O sistema vem previamente configurado com 14 computadores. Isso quer dizer
+            que, em um laboratório só será permitido agendar 14 matrículas em um mesmo
+            horário. Por exemplo, no horário das 14h às 15h só será permitido agendar 14
+            alunos. 
+        </p>
+    `, `
+        Nova matrícula, matricula, novo, Aluno, Cadastro, horário, horario, inserir
+    `,
+    "http://www.videostutoriais.grupooportunidade.com.br/suporte/videos/sistema/2.html"
+    ],
 ]
--- static/js/script.js
+++ static/js/script.js
@@ -40,7 +40,54 @@ function render_text(id){
                 }
                 text_content.innerHTML += textos[i][1]
             }else{
-                list_content.innerHTML = textos[i][0]
+                list_content.innerHTML = `<h4 class="m-2">${textos[i][0]}</h4><hr>`
+                if(textos[i][3] != ""){
+                    list_content.innerHTML += `
+                    <a href="${textos[i][3]}" type="button" class="btn btn-primary mb-3" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-btn-fill" viewBox="0 0 16 16">
+                            <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"></path>
+                        </svg>
+                        Vídeo explicativo
+                    </a>
+                    `
+                }
+                list_content.innerHTML += textos[i][1]
+            }
+            break;
+        }
+    }
+
+}
+
+function render_text_txt(txt){
+
+    for(var i = 0; i < textos.length; i++){
+        if(textos[i][0] == txt){
+            if(celular == false){
+                text_content.innerHTML = `<h4 class="m-2">${textos[i][0]}</h4><hr>`
+                if(textos[i][3] != ""){
+                    text_content.innerHTML += `
+                    <a href="${textos[i][3]}" type="button" class="btn btn-primary mb-3" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-btn-fill" viewBox="0 0 16 16">
+                            <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"></path>
+                        </svg>
+                        Vídeo explicativo
+                    </a>
+                    `
+                }
+                text_content.innerHTML += textos[i][1]
+            }else{
+                list_content.innerHTML = `<h4 class="m-2">${textos[i][0]}</h4><hr>`
+                if(textos[i][3] != ""){
+                    list_content.innerHTML += `
+                    <a href="${textos[i][3]}" type="button" class="btn btn-primary mb-3" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-btn-fill" viewBox="0 0 16 16">
+                            <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"></path>
+                        </svg>
+                        Vídeo explicativo
+                    </a>
+                    `
+                }
                 list_content.innerHTML += textos[i][1]
             }
             break;
